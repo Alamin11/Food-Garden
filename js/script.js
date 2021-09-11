8bd928eff31e3ceb0200bd838fd4b722c94f0706
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-09-11 17:08:31 +0600

    Add heading style complete

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 let menuBar = document.querySelector('#menu-bar');
 let navBar = document.querySelector('.navbar');
 
-menuBar.onclick = () => {
+menuBar.onclick = () =>{
     menuBar.classList.toggle('fa-times');
     navBar.classList.toggle('active');
+    // console.log("ok");
+}
+
+window.onscroll = () => {
+    menuBar.classList.remove('fa-times');
+    navBar.classList.remove('active');
 }
